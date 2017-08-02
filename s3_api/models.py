94411a36f38e386613b12242b67230d12934b855
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-08-02 23:38:04 +0100

    Added Customer model so as to create a relationship between customers and accounts

--- s3_api/models.py
+++ s3_api/models.py
@@ -7,8 +7,21 @@ class Account(models.Model):
     id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False
     )
+    customer = models.ForeignKey('Customer', on_delete=models.CASCADE)
     firstname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     telephone = models.CharField(max_length=100)
     balance = models.FloatField()
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Customer(models.Model):
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False
+    )
+
+    def __str__(self):
+        return str(self.id)
