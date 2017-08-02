367de4a69ca07c342e97c794dfc494eddbe05e74
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-08-03 00:04:24 +0100

    Added URL conf to align with the example request URL format

--- mvf/urls.py
+++ mvf/urls.py
@@ -1,6 +1,16 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
+from rest_framework import routers
+
+from s3_api import views
+
+router = routers.DefaultRouter()
+router.register(r'account', views.AccountViewSet)
+router.register(r'customer', views.CustomerViewSet)
+
 urlpatterns = [
+    url(r'^api/v1/', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
 ]
