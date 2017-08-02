003af58af4df11fa92626d70071ae274cec4eaae
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-08-02 23:09:51 +0100

    Added model in which to store account data and added corresponding import functionality to the data import management command

--- s3_api/management/commands/import_data.py
+++ s3_api/management/commands/import_data.py
@@ -1,7 +1,10 @@
 import boto3
 import pprint
+import json
 
 from django.core.management.base import BaseCommand
+
+from s3_api.models import Account
 
 
 class Command(BaseCommand):
@@ -12,9 +15,32 @@ class Command(BaseCommand):
         client = boto3.client('s3', region_name='eu-west-1')
         objs = client.list_objects(Bucket=bucket)
         
+        # Download each JSON file
         for num, obj in enumerate(objs['Contents']):
             file_name = '{}-{}'.format(num + 1, obj['Key'])
             s3 = boto3.resource('s3')
             s3.Bucket(bucket).download_file(
                 obj['Key'], file_name
             )
+
+            self.import_data(file_name)
+
+    def import_data(self, file_name):
+        """ Import a given locally stored file in to the Account model """
+
+        with open(file_name) as account_info:
+            accounts = json.load(account_info)['accounts']
+
+            for account in accounts:
+                # We don't want commas in the balance
+                balance = str(account['balance']).replace(',', '')
+
+                # Could look in to bulk_create later to improve efficiency
+                Account.objects.create(
+                    id=account['id'],
+                    firstname=account['firstname'],
+                    lastname=account['lastname'],
+                    email=account['email'],
+                    telephone=account['telephone'],
+                    balance=float(balance),
+                )
--- s3_api/models.py
+++ s3_api/models.py
@@ -1,3 +1,14 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False
+    )
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    telephone = models.CharField(max_length=100)
+    balance = models.FloatField()
