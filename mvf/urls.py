6fb01fb4d51ea91111cf9ad0b47173be809605cf
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-08-03 01:07:34 +0100

    Added urls and views to satisfy the main requirements now that all the backend prep work has been done, working very nicely

--- mvf/urls.py
+++ mvf/urls.py
@@ -5,12 +5,21 @@ from rest_framework import routers
 
 from s3_api import views
 
+
 router = routers.DefaultRouter()
 router.register(r'account', views.AccountViewSet)
 router.register(r'customer', views.CustomerViewSet)
 
 urlpatterns = [
     url(r'^api/v1/', include(router.urls)),
+    url(
+        r'^api/v1/customer/(?P<guid>[^/.]+)/accounts/$',
+        views.CustomerViewSet.as_view({'get': 'customer_accounts'})
+    ),
+    url(
+        r'^api/v1/account/(?P<guid>[^/.]+)/(?P<field>[^/.]+)/$',
+        views.AccountViewSet.as_view({'get': 'account_field'})
+    ),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
 ]
--- s3_api/views.py
+++ s3_api/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import detail_route
 
-# Create your views here.
+from s3_api.serializers import AccountSerializer, CustomerSerializer
+from s3_api.models import Account, Customer
+
+
+class AccountViewSet(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+    def account_field(self, request, guid, field):
+        account = Account.objects.get(id=guid)
+        serializer = AccountSerializer(account)
+        return Response(serializer.data.get(
+            field, 'Not a valid field name.'
+        ))
+
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+    def customer_accounts(self, request, guid):
+        """ Return only accounts associated with the given customer
+            guid (treated as guid)
+        """
+        accounts = Account.objects.filter(customer__id=guid)
+        serializer = AccountSerializer(accounts, many=True)
+        return Response(serializer.data)
